d893a5facbcf0788d2ab4802c6103f87586f11ee
yashkhem1 <yashkhem1@gmail.com>
2020-09-21 09:31:45 +0530

    Translation Statistics

--- create_pseudo_translation.py
+++ create_pseudo_translation.py
@@ -4,6 +4,7 @@ import random
 import pickle
 import string
 from tqdm import tqdm
+import progressbar
 
 def create_pseudo_translation_oscar(l1,l2):
     dictionary = pickle.load(open(os.path.join('Bilingual_Mappings',l1+'_'+l2+'_dict.pkl'),'rb'))
@@ -34,7 +35,48 @@ def create_pseudo_translation_oscar(l1,l2):
                 w.write(translation[:-1])
 
 
+def get_translation_statistics(l1,l2):
+    dictionary = pickle.load(open(os.path.join('Bilingual_Mappings',l1+'_'+l2+'_dict.pkl'),'rb'))
+    dict_keys = list(dictionary.keys())
+    dict_keys_used = set()
+    total_words = 0
+    words_translated = 0
+    monolingual_file = l1+'_sentences.txt'
+    pseudo_translation = l2+'_pseudo_'+l1+'.txt'
+    bar = progressbar.ProgressBar()
+    bar = progressbar.ProgressBar(term_width = 20,suffix="Total Dict: {variables.total_dict} Used Dict: {variables.dict_used} Used%: {variables.used_perc} Total: {variables.total_words} Translated: {variables.translated}",
+                                                                        variables = {'total_dict':'--','dict_used':'--','used_perc':'--','total_words':'--','translated':'--'})
+    i = 0
+    with open(monolingual_file,'r') as f1:
+        with open(pseudo_translation,'r') as f2:
+            line_l1 = next(f1)
+            line_l2 = next(f2)
+            while(line_l1 and line_l2):
+                words_l1 = line_l1.split(" ")
+                words_l2 = line_l2.split(" ")
+                assert(len(words_l1)==len(words_l2))
+                for (word_l1,word_l2) in zip(words_l1,words_l2):
+                    total_words += 1
+                    if word_l1 != word_l2:
+                        words_translated+=1
+                        if word_l1.endswith('\n'):
+                            word_l1 = word_l1[:-1]
+                        punctuations = ['...']+list(string.punctuation)
+                        for punctuation in punctuations:
+                            if word_l1.endswith(punctuation):
+                                word_l1 = word_l1[:-1*len(punctuation)]
+                        dict_keys_used.add(word_l1)
+                # print(len(dict_keys_used))
+                i+=1 
+                bar.update(i+1,total_dict=str(len(dict_keys)),dict_used=str(len(dict_keys_used)),used_perc=str(len(dict_keys_used)/len(dict_keys)),
+                                total_words=str(total_words),translated=str(words_translated))
+                bar.update(i+1)
+                line_l1 = next(f1)
+                line_l2 = next(f2)
+
+
 if __name__=="__main__":
     l1 = sys.argv[1]
     l2 = sys.argv[2]
-    create_pseudo_translation_oscar(l1,l2)
+    # create_pseudo_translation_oscar(l1,l2)
+    get_translation_statistics(l1,l2)
