cdeba160224b29a37e5196240b7c7c6f175fe67a
Yash Khemchandani <yashkhem1@gmail.com>
2020-10-15 22:55:21 +0000

    Json instead of pickle

--- utils.py
+++ utils.py
@@ -38,48 +38,34 @@ def tokenize_with_mappings(l1,l2,tokenizer,word_mapping = None):
 
 def preprocess_with_mappings(file1,file2,outfile,max_length,tokenizer,document_information=True,word_mapping = None):
     bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength)
-    token_ids_f1 = []
-    token_ids_f2 = []
-    attention_masks_f1 = []
-    attention_masks_f2 = []
-    token_maps_f1 = []
-    token_maps_f2 = []
-    weight_maps_f1 = []
-    weight_maps_f2 = []
-
     with open(file1,'r') as f1:
         with open(file2,'r') as f2:
+            with open(outfile,'a') as w:
+                _ = w.write('{\n')
+                _ = w.write('\t"data":[\n')
                 line1 = f1.readline()
                 line2 = f2.readline()
                 i=0
+                first = 1
                 ids_1 = [101]
                 ids_2 = [101]
-                masks_1 = [1]
-                masks_2 = [1]
                 tok_maps_1 = []
                 tok_maps_2 = []
                 while line1:
                     if document_information:
                         token_ids_l1, token_ids_l2, token_maps_l1, token_maps_l2 = tokenize_with_mappings(line1,line2,tokenizer,word_mapping)
-                        # print(token_ids_l1)
                         if (len(ids_1) + len(token_ids_l1) > max_length) or (len(ids_2) + len(token_ids_l2) > max_length) or line1=="\n":
                             if not (len(ids_1)==1 or len(ids_2)==1):
                                 ids_1 += [102]+[0]*(max_length-len(ids_1)-1)
                                 ids_2 += [102]+[0]*(max_length-len(ids_2)-1)
-                                masks_1 += [1]+[0]*(max_length-len(masks_1)-1)
-                                masks_2 += [1]+[0]*(max_length-len(masks_2)-1)
-                                weight_maps_1 = [1]*len(tok_maps_1) + [0]*(max_length-len(tok_maps_1))
-                                weight_maps_2 = [1]*len(tok_maps_2) + [0]*(max_length-len(tok_maps_2))
                                 tok_maps_1 += [0]*(max_length-len(tok_maps_1))
                                 tok_maps_2 += [0]*(max_length-len(tok_maps_2))
-                                token_ids_f1.append(ids_1)
-                                token_ids_f2.append(ids_2)
-                                attention_masks_f1.append(masks_1)
-                                attention_masks_f2.append(masks_2)
-                                weight_maps_f1.append(weight_maps_1)
-                                weight_maps_f2.append(weight_maps_2)
-                                token_maps_f1.append(tok_maps_1)
-                                token_maps_f2.append(tok_maps_2)
+                                if first:
+                                    _ = w.write('\t\t{"token_ids_1":'+str(ids_1)+',"token_ids_2":'+str(ids_2)+',"token_maps_1":'+str(tok_maps_1)+',"token_maps_2":'+str(tok_maps_2)+'}')
+                                    first = 0
+                                
+                                else:
+                                    _ = w.write(',\n\t\t{"token_ids_1":'+str(ids_1)+',"token_ids_2":'+str(ids_2)+',"token_maps_1":'+str(tok_maps_1)+',"token_maps_2":'+str(tok_maps_2)+'}')
 
                             if line1=="\n" or len(ids_1)==1 or len(ids_2)==1:
                                 line1 = f1.readline()
@@ -89,34 +75,21 @@ def preprocess_with_mappings(file1,file2,outfile,max_length,tokenizer,document_i
                             
                             ids_1 = [101]
                             ids_2 = [101]
-                            masks_1 = [1]
-                            masks_2 = [1]
                             tok_maps_1 = []
                             tok_maps_2 = []
                         
                         else:
-                            ids_1 += token_ids_l1
-                            ids_2 += token_ids_l2
-                            masks_1 += [1]*len(token_ids_l1)
-                            masks_2 += [1]*len(token_ids_l2)
                             tok_maps_1 += [x+len(ids_1) for x in token_maps_l1]
                             tok_maps_2 += [x+len(ids_2) for x in token_maps_l2]
+                            ids_1 += token_ids_l1
+                            ids_2 += token_ids_l2
                             line1 = f1.readline()
                             line2 = f2.readline()
                             bar.update(i)
                             i+=1
     
-    with open(outfile,'wb') as f:
-        out_dict = {}
-        out_dict['token_ids_f1'] = token_ids_f1
-        out_dict['token_ids_f2'] = token_ids_f2
-        out_dict['attention_masks_f1'] = attention_masks_f1
-        out_dict['attention_masks_f2'] = attention_masks_f2
-        out_dict['token_maps_f1'] = token_maps_f1
-        out_dict['token_maps_f2'] = token_maps_f2
-        out_dict['weight_maps_f1'] = weight_maps_f1
-        out_dict['weight_maps_f2'] = weight_maps_f2
-        pickle.dump(out_dict,f)
+                _ = w.write('\n\t]\n')
+                _ = w.write('}')
 
 
 if __name__ == "__main__":
@@ -133,7 +106,7 @@ if __name__ == "__main__":
     if corpus == 'Wikipedia':
         file1 = os.path.join('Wikipedia',l1+'.txt')
         file2 = os.path.join('Wikipedia',l2+'_pseudo_'+l1+'.txt')
-        outfile = l1 + '_' + l2 +'.pkl'
+        outfile = l1 + '_' + l2 +'.json'
         preprocess_with_mappings(file1,file2,outfile,max_length,tokenizer)
                     
 
